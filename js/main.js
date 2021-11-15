074344f71747587a8f4ed772044777d85fef2eb0
goffreyOdari <geoffrey.ochenge@gmail.com>
2021-11-15 19:25:06 +0300

    Added inputs and logic

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ function getDayOfWeek   (y,m,d){
     return Math.round((y + y/4 - y/100 + y/400 + t[m-1] + d) % 7);
 }
 
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
+const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+let gender = prompt("Please enter your gender ");
+let yearOfBirth = prompt("Please enter year of birth: ");
+let monthOfBirth = prompt("Please enter month ");
+let dateOfBirth = prompt("Please enter date ");
+
+
+console.log(getDayOfWeek(parseInt(yearOfBirth),parseInt(monthOfBirth),parseInt(dateOfBirth)))
+
+if(gender.toLowerCase()=="male"){
+    alert(maleNames[getDayOfWeek(parseInt(yearOfBirth),parseInt(monthOfBirth),parseInt(dateOfBirth))])
+}else if (gender.toLowerCase()=="female") {
+    alert(femaleNames[getDayOfWeek(parseInt(yearOfBirth),parseInt(monthOfBirth),parseInt(dateOfBirth))])
+}
+
