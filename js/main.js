a36fd70a2802f343b860d123acceb52c8ac16d06
goffreyOdari <geoffrey.ochenge@gmail.com>
2021-11-22 21:48:08 +0300

    Moved the element variables outside the submit event listener

--- js/main.js
+++ js/main.js
@@ -8,21 +8,21 @@ function getDayOfWeek   (y,m,d){
 const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi"];
 const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
+const  gender= document.querySelector("#gender")//prompt("Please enter your gender ");
+const yearOfBirth = document.querySelector("#year")//prompt("Please enter year of birth: ");
+const monthOfBirth = document.querySelector("#month")//prompt("Please enter month ");
+const dateOfBirth = document.querySelector("#date")//prompt("Please enter date ");
 
 
 
 document.querySelector("#submit").addEventListener("click",()=>{
-    let gender = document.querySelector("#gender").value//prompt("Please enter your gender ");
-    let yearOfBirth = document.querySelector("#year").value//prompt("Please enter year of birth: ");
-    let monthOfBirth = document.querySelector("#month").value//prompt("Please enter month ");
-    let dateOfBirth = document.querySelector("#date").value//prompt("Please enter date ");
-    console.log(getDayOfWeek(parseInt(yearOfBirth),parseInt(monthOfBirth),parseInt(dateOfBirth)))
+    
+    console.log(getDayOfWeek(parseInt(yearOfBirth.value),parseInt(monthOfBirth.value),parseInt(dateOfBirth.value)))
 
-        if(gender.toLowerCase()=="male"){
-            alert(maleNames[getDayOfWeek(parseInt(yearOfBirth),parseInt(monthOfBirth),parseInt(dateOfBirth))])
-        }else if (gender.toLowerCase()=="female") {
-            alert(femaleNames[getDayOfWeek(parseInt(yearOfBirth),parseInt(monthOfBirth),parseInt(dateOfBirth))])
+        if(gender.value.toLowerCase()=="male"){
+            alert(maleNames[getDayOfWeek(parseInt(yearOfBirth.value),parseInt(monthOfBirth.value),parseInt(dateOfBirth.value))])
+        }else if (gender.value.toLowerCase()=="female") {
+            alert(femaleNames[getDayOfWeek(parseInt(yearOfBirth.value),parseInt(monthOfBirth.value),parseInt(dateOfBirth.value))])
         }
 
 })
